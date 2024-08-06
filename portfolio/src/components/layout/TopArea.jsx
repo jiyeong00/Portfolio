56b87876f4cc40eee7c615ce6ff44d29faf3e456
ji0 <buti12@naver.com>
2024-08-06 14:24:39 +0900

    ㄴ

---
+++ portfolio/src/components/layout/TopArea.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TopArea(props) {
+    return (
+        <div>
+            하이요
+        </div>
+    );
+}
+
+export default TopArea;
--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,18 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+
 import "./index.css";
 
 // 컴포넌트 불러오기
+import Layout from "./components/layout/Layout";
 import Main from "./components/page/Main";
 
 
 export default function MainComponent() {
-  <Router>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <Routes>
     <Route path="/" element={<Layout />}>
       <Route index element={<Main />} />
-      <Route path="character" element={<Character />} />
     </Route>
-  </Router>;
+  </Routes>
+  </BrowserRouter>
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
