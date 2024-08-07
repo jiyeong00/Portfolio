b8e1ab42dba6dbcb0103debd8c955970fe53a68f
ji0 <buti12@naver.com>
2024-08-07 15:31:50 +0900

    ㄴㄴㄴ

--- portfolio-react/src/components/pages/Main.jsx
+++ portfolio-react/src/components/pages/Main.jsx
@@ -1,54 +1,95 @@
 import React from "react";
 import Projects from "./Projects";
 
+// css
+import "../../css/main.scss";
+
+// 폰트어썸
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCalendar,
+  faEnvelope,
+  faLocationDot,
+  faPen,
+  faPhoneVolume,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+
 function Main(props) {
   return (
     <section className="mainArea">
       <div className="part main">
-        <div className="main-wrap">
+        <div className="main-wrap wrap ">
           <h1>신입 프론트엔드 개발자 포트폴리오</h1>
+          <hr className="hr-tag"></hr>
           <h2>
             안녕하세요
             <br />
-            꿈을 향해 달려가는 개발자 유지영입니다.
+            꿈을 향해 달려가는 개발자, <b>유지영</b>입니다.
           </h2>
         </div>
         {/* <!-- main-wrap --> */}
       </div>
       {/* <!-- main --> */}
       <div className="part about">
-        <div className="about-wrap">
-          <div className="about-tit">ABOUT ME</div>
+        <div className="about-wrap wrap">
+          <div className="about-tit core-title">
+            <span>ABOUT ME</span>
+          </div>
           <ul className="about-list">
             <li>
-              <div className="icon"></div>
-              <strong>이름</strong>
-              <span>유지영</span>
+              <div className="list-wrap">
+                <FontAwesomeIcon className="font-awesome" icon={faUser} />
+                <div className="about-info">
+                  <strong>이름</strong>
+                  <span>유지영</span>
+                </div>
+              </div>
             </li>
             <li>
-              <div className="icon"></div>
-              <strong>생년월일</strong>
-              <span>99.12.06</span>
+              <div className="list-wrap">
+                <FontAwesomeIcon className="font-awesome" icon={faCalendar} />
+                <div className="about-info">
+                  <strong>생년월일</strong>
+                  <span>99.12.06</span>
+                </div>
+              </div>
             </li>
             <li>
-              <div className="icon"></div>
-              <strong>주소지</strong>
-              <span>서울특별시 성동구</span>
+              <div className="list-wrap">
+                <FontAwesomeIcon className="font-awesome" icon={faLocationDot} />
+                <div className="about-info">
+                  <strong>주소지</strong>
+                  <span>서울특별시 성동구</span>
+                </div>
+              </div>
             </li>
             <li>
-              <div className="icon"></div>
-              <strong>연락처</strong>
-              <span>010-3449-5231</span>
+              <div className="list-wrap">
+                <FontAwesomeIcon className="font-awesome" icon={faPhoneVolume} />
+                <div className="about-info">
+                  <strong>연락처</strong>
+                  <span>010-3449-5231</span>
+                </div>  
+              </div>
             </li>
             <li>
-              <div className="icon"></div>
-              <strong>이메일</strong>
-              <span>buti12@naver.com</span>
+              <div className="list-wrap">
+                <FontAwesomeIcon className="font-awesome" icon={faEnvelope} />
+                <div className="about-info">
+                  <strong>이메일</strong>
+                  <span>buti12@naver.com</span>
+                </div>
+              </div>
             </li>
             <li>
-              <div className="icon"></div>
-              <strong>학력</strong>
-              <span>배화여자대학교(정보보호과/SW보안융합학과)</span>
+              <div className="list-wrap">
+                <FontAwesomeIcon className="font-awesome" icon={faPen} />
+                <div className="about-info">
+                  <strong>학력</strong>
+                  <span>배화여자대학교<br/>SW보안융합학과</span>
+                </div>
+              </div>
             </li>
           </ul>
           <div className="about-git">
@@ -61,11 +102,13 @@ function Main(props) {
       </div>
       {/* <!-- about me --> */}
       <div className="part skills">
-        <div className="skills-wrap">
-          <div className="skills-tit">SKILLS</div>
+        <div className="skills-wrap wrap">
+          <div className="skills-tit core-title">
+            <span>SKILLS</span>
+          </div>
           <ul>
             <li>
-              <h3>업무에도 활용 가능해요!</h3>
+              <h3>Frontend</h3>
               <ol>
                 <li>HTML</li>
                 <li>CSS</li>
@@ -76,26 +119,18 @@ function Main(props) {
             </li>
             <li>
               <ol>
-                <h3>어느 정도 사용할 줄 알아요.</h3>
+                <h3>Backend</h3>
                 <li>pSQL</li>
                 <li>mySQL</li>
                 <li>jQuery</li>
               </ol>
             </li>
-            <li>
-              <ol>
-                <h3>보고 이해할 수는 있어요.</h3>
-                <li>C</li>
-                <li>파이선</li>
-                <li>JAVA</li>
-                <li>Vue.JS</li>
-                <li>PHP</li>
-              </ol>
-            </li>
           </ul>
         </div>
+        {/* skills-wrap */}
       </div>
-      <Projects/>
+      {/* skills */}
+      <Projects />
     </section>
   );
 }
--- portfolio-react/src/components/pages/Projects.jsx
+++ portfolio-react/src/components/pages/Projects.jsx
@@ -2,11 +2,48 @@ import React from "react";
 
 function Projects(props) {
   return (
-    <div className="projects">
-      <div className="projects-wrap">
-        프로젝트
+    <div className="part projects">
+      <div className="projects-wrap wrap">
+        <div className="projects-tit core-title">
+          <span>PROJECTS</span>
+        </div>
+        <div className="projects-box">
+          <h2>ShinHan Art</h2>
+          <span>2024.06 ~ (1人 개인 프로젝트)</span>
+          <div className="projects-info">
+            {/* 스와이퍼 넣을 자리 */}
+            <div className="swiper-img">
+              <img src="/img/1 (1).jpg" alt="으아아아" />
+            </div>
+            <div className="projects-txt">
+              <span>
+                타임박스 플래너를 앱으로 활용하기 위한 PWA 기반의
+                웹사이트입니다. 시중의 앱들은 필요 이상으로 복잡하여 타임박스
+                플래너 본연의 기능만을 최소한으로 담도록 직접 개발하였습니다.
+                조금씩 공부하고 있었던 새로운 버전의 Next.js를 본격적으로 체화할
+                수 있었습니다. 인증의 구현을 위해 Supabase를 처음 사용해보며
+                Firebase와 비교해볼 수 있었고, Vercel의 무료 PostgreSQL을
+                사용해보며 Raw SQL에 더욱 익숙해질 수 있었습니다. 지원이 중단된
+                Recoil 대신에 Zustand를 처음으로 익히면서 Recoil과의 차이점을
+                느낄 수 있었습니다.
+              </span>
+              <div className="projects-desc">
+                <strong>주요기능</strong>
+                <span>
+                  날짜별로 각 루틴의 달성 여부, 할 일 목록, 시간 단위 일정
+                  기입하기, 월별로 각 루틴의 달성 여부를 한눈에 확인하기
+                </span>
+              </div>
+            </div>
+            {/* projects-txt */}
+          </div>
+          {/* projects-info */}
+        </div>
+        {/* projects-box */}
       </div>
+      {/* <!-- projects-wrap --> */}
     </div>
+    // part
   );
 }
 
