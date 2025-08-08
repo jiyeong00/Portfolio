b030cecf6676c397743f1812ec1cf2c00890d3cc
jiyeong00 <159221424+jiyeong00@users.noreply.github.com>
2025-08-08 20:00:42 +0900

    느아아

--- portfolio-react/src/components/data/project_img_data.js
+++ portfolio-react/src/components/data/project_img_data.js
@@ -1,15 +1,15 @@
 const imgData = [
   {
     title: "Shinhan art",
-    img:["Shinhan1.png","Shinhan2.png","Shinhan3.png","Shinhan4.png","Shinhan5.png","Shinhan6.png",],
+    img:["Shinhan1.png","Shinhan2.png","Shinhan3.png","Shinhan4.png","Shinhan5.png","Shinhan6.png"],
   },
     {
     title: "Sola Sysyem",
-    img:["4.jpg","5.jpg","6.jpg"],
+    img:["Sola1.png","Sola2.png","Sola3.png"],
   },
     {
     title: "보담보담",
-    img:["1.jpg","2.jpg","3.jpg"],
+    img:["보담1.png","보담2.png","보담3.png","보담4.png","보담5.png","보담6.png","보담7.png"],
   },
 
 ]
